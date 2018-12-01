0410580ea315cecc25f6f12e6eb93e6ab7302eb9
MasanoriIwakura <hagobutler@yahoo.co.jp>
2018-12-01 18:55:33 +0900

    Add Google Analytics Setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,13 @@
 module.exports = {
   title: 'Iwakura Vue Blog',
   description: '〜頑張ってアウトプットします宣言〜',
+  ga: 'UA-104484317-2',
   locales: {
     '/': {
       lang: 'ja'
     }
   },
+  extend: '',
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -16,6 +18,7 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'Profile', link: '/profile/' },
       { text: 'Diary', link: '/dialy/' }
-    ]
+    ],
+    lastUpdated: 'Last Updated'
   }
 }
